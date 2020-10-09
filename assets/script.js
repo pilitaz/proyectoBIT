09909218b09a013e7e0441051f5ba2bc15be9d5a
pilitaz <jennyp.gomezb@hotmail.com>
2020-10-08 21:29:01 -0500

    cambios al index

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,17 @@
-const comoFunciona = document.querySelector('#comoFunciona');
+
 const buscaTuMascota = document.querySelector('#buscaTuMascota');
 const reportaUnaMascota = document.querySelector('#reportaUnaMascota');
 const casosFelices = document.querySelector('#casosFelices');
-const sobreNosotros = document.querySelector('#sobreNosotros');
+// const sobreNosotros = document.querySelector('#sobreNosotros');
 const contenidoIframe = document.querySelector('#contenidoIframe');
 
 window.addEventListener('resize', reportWindowSize);
 
 function reportWindowSize() {
   let heightOriginal = window.innerHeight;
-  let heightIframe = heightOriginal - 265;
+  let heightIframe = heightOriginal - 275;
   contenidoIframe.setAttribute('style', 'height:' + heightIframe + 'px');
 }
-
-comoFunciona.addEventListener("click", function (event) {
-  cambiarSRCIframe("comoFunciona");
-}, true);
 
 buscaTuMascota.addEventListener("click", function (event) {
   cambiarSRCIframe("lista")
@@ -29,12 +25,9 @@ casosFelices.addEventListener("click", function (event) {
   cambiarSRCIframe("casosFelices")
 }, true);
 
-sobreNosotros.addEventListener("click", function (event) {
-  cambiarSRCIframe("sobreNosotros");
-}, true);
-
 function cambiarSRCIframe(src) {
   document.getElementById("contenidoIframe").src = "html/" + src + ".html";
 }
 
-reportWindowSize();
+const eventResize = new Event('resize');
+window.dispatchEvent(eventResize);
